99993f421fcb6b6bcc2a10d28847c246b2bdb7e0
Franck Duché <franckduche@gmail.com>
2014-02-07 16:08:12 +0100

    add response handling in request

--- src/App.php
+++ src/App.php
@@ -5,6 +5,7 @@ use Exception\HttpException;
 use Routing\Route;
 use View\TemplateEngineInterface;
 use Http\Request;
+use Http\Response;
 
 class App
 {
@@ -114,11 +115,16 @@ class App
     private function process(Request $request, Route $route)
     {
         try {
-            http_response_code($this->statusCode);
             $arguments = $route->getArguments();
 			array_unshift($arguments, $request);
+			
 			$response = call_user_func_array($route->getCallable(), $arguments);
-			echo $response;
+			
+			if (!$response instanceof Response) {
+				$response = new Response($response);
+			}
+			
+			$response->send();
         } catch (HttpException $e) {
             throw $e;
         } catch (\Exception $e) {
