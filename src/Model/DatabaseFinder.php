68e62bce66a69b5233d67eb6b6f5e7a176b84a13
Franck Duché <franckduche@gmail.com>
2014-02-07 16:13:09 +0100

    add new finder using connection

---
+++ src/Model/DatabaseFinder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Model;
+
+use Exception\StatusNotFoundException;
+
+class JsonFinder implements FinderInterface
+{
+	private $statuses;
+	private $connection
+	
+	public function __construct($connection)
+	{
+		$this->connection = $connection;
+		$this->statuses = $this->connection->executeQuery('SELECT * FROM Statuses');
+	}
+	
+    public function findAll()
+    {
+		if (!$this->statuses) {
+			throw new StatusNotFoundException("Statuses not found.");
+		}
+		if ($this->statuses === "[]") {
+			return array();
+		}
+		
+		$arrayStatusesObjects = array();
+		foreach ($this->statuses as $arrayStatus) {
+			$arrayStatusesObjects [] = Status::fromArray($arrayStatus);
+		}
+		return $arrayStatusesObjects;
+    }
+
+    public function findOneById($id)
+    {
+		$statuses = $this->findAll();
+		$result = null;
+        foreach ($statuses as $status) {
+            if ($id === $status->getId()) {
+                $result = $status;
+            }
+        }
+        if (null === $result) {
+			throw new StatusNotFoundException("Status not found.");
+		}
+        return $result;
+    }
+}
